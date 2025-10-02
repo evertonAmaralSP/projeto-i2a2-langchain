e0273420b12ed73acd99d57819a51f4463be5fbd
evertonamaralsp <everton.amaral@gmail.com>
2025-10-02 08:24:12 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -5,14 +5,73 @@ from langchain.prompts import PromptTemplate
 from langchain.agents import create_react_agent
 from langchain.agents import AgentExecutor
 from ferramentas import criar_ferramentas
-
+import zipfile
+import io
 import os
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
+REQUIRED_COLUMNS_NOTA_FISCAL = ['CHAVE DE ACESSO', 'MODELO_x', 'SÉRIE_x', 'NÚMERO_x']
+REQUIRED_DF1_COLUMNS_NOTA_FISCAL = ['CHAVE DE ACESSO', 'MODELO', 'SÉRIE', 'NÚMERO']
+REQUIRED_COLUMNS_CREDITCARD = ['Time', 'V1', 'V28', 'Amount', 'Class']
+
+def extrair_zip_e_carregar_csvs_em_memoria(arquivo_carregado):
+    """
+    Extrai todos os arquivos CSV de um ZIP e os carrega em DataFrames Pandas em memória.
+    Retorna um dicionário onde a chave é o nome do arquivo CSV (sem caminho) e o valor é o DataFrame.
+    """
+
+    dfs = {}
+    with zipfile.ZipFile(arquivo_carregado, 'r') as zip_ref:
+        csv_files_in_zip = [name for name in zip_ref.namelist() if name.lower().endswith('.csv')]
+
+        if not csv_files_in_zip:
+            st.error("❌ Nenhum arquivo CSV encontrado dentro do ZIP.")
+        else:
+            st.success(f"✅ Encontrados {len(csv_files_in_zip)} arquivo(s) CSV no ZIP")
+                
+
+        for csv_file_name in csv_files_in_zip:
+            try:
+                with zip_ref.open(csv_file_name) as file:
+                    # Lê o arquivo CSV diretamente da memória
+                    df = pd.read_csv(io.TextIOWrapper(file, 'utf-8'))
+                    # Usa apenas o nome do arquivo, sem o caminho completo dentro do zip
+                    df_name = os.path.basename(csv_file_name)
+                    dfs[df_name] = df
+            except Exception as e:
+                print(f"Erro ao ler o CSV '{csv_file_name}' do ZIP: {e}")
+                # Opcional: ignorar arquivos CSV que não podem ser lidos ou levantar um erro mais específico
+
+    if not dfs:
+        raise ValueError("Nenhum arquivo CSV válido pôde ser carregado do ZIP.")
+
+    return dfs
+
+
+def validate_columns(dataframe, column_list):
+  return all(col in dataframe.columns for col in column_list)
+
+def carregar_arquivo_zip_to_df(arquivo_carregado):
+    dfs_dict = extrair_zip_e_carregar_csvs_em_memoria(arquivo_carregado)
+
+    df1_name = list(dfs_dict.keys())[0]
+    df2_name = list(dfs_dict.keys())[1]
+    df1 = dfs_dict[df1_name]
+    df2 = dfs_dict[df2_name]
+    if not validate_columns(df1, REQUIRED_DF1_COLUMNS_NOTA_FISCAL):
+        st.error("❌ Não é um arquivo zip de Notafiscal Valido.")
     
-st.set_page_config(page_title="Assistente de análise de dados com IA", layout="centered")
-st.title("🦜 Assistente de análise de dados com IA")
+    try:
+        merged_df = pd.merge(df1, df2, on='CHAVE DE ACESSO', how='inner')
+    except KeyError:
+        st.error("❌ Não é uma combinação de arquivos csv de Notafiscal Valido.")
+        return None
+    return merged_df
+
+
+st.set_page_config(page_title="Assistente de Elite IA", layout="centered")
+st.title("🧠 Assistente de Elite IA 🦜")
 
 # Descrição da ferramenta
 st.info("""
@@ -21,17 +80,25 @@ Este assistente utiliza um agente, criado com Langchain, para te ajudar a explor
 
 # Upload do CSV
 st.markdown("### 📁 Faça upload do seu arquivo CSV")
-arquivo_carregado = st.file_uploader("Selecione um arquivo CSV", type="csv", label_visibility="collapsed")
+arquivo_carregado = st.file_uploader("Selecione um arquivo CSV", type=["csv", "zip"], label_visibility="collapsed")
 
 if arquivo_carregado:
-    df = pd.read_csv(arquivo_carregado)
-    st.success("Arquivo carregado com sucesso!")
-    st.markdown("### Primeiras linhas do DataFrame")
-    st.dataframe(df.head())
-
-    # LLM
-
-
+    nome_arquivo = arquivo_carregado.name
+    if nome_arquivo.lower().endswith('.zip'):
+        df = carregar_arquivo_zip_to_df(arquivo_carregado)
+    else:
+        df = pd.read_csv(arquivo_carregado)    
+    
+    if validate_columns(df, REQUIRED_COLUMNS_NOTA_FISCAL):
+      tipo = "Nota Fiscal"
+    elif validate_columns(df, REQUIRED_COLUMNS_CREDITCARD):
+      tipo = "Credit Card"
+    else:
+      tipo = "Dados Comuns"
+      
+    st.success("Arquivo carregado com sucesso!")  
+    st.markdown(f"Arquivo do tipo: {tipo}")
+    
     llm = ChatOpenAI(
         model="gpt-4o",
         temperature=0.1,
@@ -68,7 +135,7 @@ if arquivo_carregado:
         Action: a ação a ser tomada, deve ser uma das [{tool_names}]  
         Action Input: a entrada para a ação  
         Observation: o resultado da ação  
-        ... (este Thought/Action/Action Input/Observation pode se repetir N vezes)
+        ... (este Thought/Action/Action Input/Observation pode se repetir 5 vezes)
         Thought: Agora eu sei a resposta final  
         Final Answer: a resposta final para a pergunta de entrada original.
         Quando usar a ferramenta_python: formate sua resposta final de forma clara, em lista, com valores separados por vírgulas e duas casas decimais sempre que apresentar números.
@@ -86,62 +153,13 @@ if arquivo_carregado:
                                 tools=tools,
                                 verbose=True,
                                 handle_parsing_errors=True)
-
-    # AÇÕES RÁPIDAS
-    st.markdown("---")
-    st.markdown("## ⚡ Ações rápidas")
-
-    # Relatório de informações gerais
-    if st.button("📄 Relatório de informações gerais", key="botao_relatorio_geral"):
-        with st.spinner("Gerando relatório 🦜"):
-            resposta = orquestrador.invoke({"input": "Quero um relatório com informações sobre os dados"})
-            st.session_state['relatorio_geral'] = resposta["output"]
-
-    # Exibe o relatório com botão de download
-    if 'relatorio_geral' in st.session_state:
-        with st.expander("Resultado: Relatório de informações gerais"):
-            st.markdown(st.session_state['relatorio_geral'])
-
-            st.download_button(
-                label="📥 Baixar relatório",
-                data=st.session_state['relatorio_geral'],
-                file_name="relatorio_informacoes_gerais.md",
-                mime="text/markdown"
-            )
-
-    # Relatório de estatísticas descritivas
-    if st.button("📄 Relatório de estatísticas descritivas", key="botao_relatorio_estatisticas"):
-        with st.spinner("Gerando relatório 🦜"):
-            resposta = orquestrador.invoke({"input": "Quero um relatório de estatísticas descritivas"})
-            st.session_state['relatorio_estatisticas'] = resposta["output"]
-
-    # Exibe o relatório salvo com opção de download
-    if 'relatorio_estatisticas' in st.session_state:
-        with st.expander("Resultado: Relatório de estatísticas descritivas"):
-            st.markdown(st.session_state['relatorio_estatisticas'])
-
-            st.download_button(
-                label="📥 Baixar relatório",
-                data=st.session_state['relatorio_estatisticas'],
-                file_name="relatorio_estatisticas_descritivas.md",
-                mime="text/markdown"  
-            )
    
    # PERGUNTA SOBRE OS DADOS
     st.markdown("---")
-    st.markdown("## 🔎 Perguntas sobre os dados")
-    pergunta_sobre_dados = st.text_input("Faça uma pergunta sobre os dados (ex: 'Qual é a média do tempo de entrega?')")
+    st.markdown("## 🔎 Perguntas sobre os dados ou peça um 📊 gráfico com base em uma pergunta")
+    
+    pergunta_sobre_dados = st.text_input("Faça uma pergunta sobre os dados (ex: 'Qual é a média do tempo de entrega?' ou 'Crie um gráfico da média de tempo de entrega por clima.')")
     if st.button("Responder pergunta", key="responder_pergunta_dados"):
         with st.spinner("Analisando os dados 🦜"):
             resposta = orquestrador.invoke({"input": pergunta_sobre_dados})
             st.markdown((resposta["output"]))
-
-
-    # GERAÇÃO DE GRÁFICOS
-    st.markdown("---")
-    st.markdown("## 📊 Criar gráfico com base em uma pergunta")
-
-    pergunta_grafico = st.text_input("Digite o que deseja visualizar (ex: 'Crie um gráfico da média de tempo de entrega por clima.')")
-    if st.button("Gerar gráfico", key="gerar_grafico"):
-        with st.spinner("Gerando o gráfico 🦜"):
-            orquestrador.invoke({"input": pergunta_grafico})
